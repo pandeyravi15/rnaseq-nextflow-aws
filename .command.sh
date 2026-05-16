002ec09031f8fa3e331120da3b8c22e5077db03c
Ravi Shanker Pandey <ravisp1587@gmail.com>
2026-05-16 20:53:52 +0000

    Add .gitignore and remove unnecessary files

--- .command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-echo "Uploading to S3..."
-aws s3 sync . s3://ravi-pandey-bioinformatics/rnaseq/results/nextflow/
-echo "Upload complete!"
-aws s3 ls s3://ravi-pandey-bioinformatics/rnaseq/results/nextflow/ --human-readable
--- work/a3/903c8ce2e55d484aacaf7125156a13/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-echo "Downloading counts matrix from S3..."
-aws s3 cp s3://ravi-pandey-bioinformatics/rnaseq/results/counts/5XFAD_counts.txt counts_matrix.txt
-echo "Done: $(wc -l < counts_matrix.txt) lines"
